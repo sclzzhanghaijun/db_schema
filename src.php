9eaed9d17e98a07088abe10451cc51e439df071a
xiaohai <sclzzhanghaijun@163.com>
2020-05-25 17:48:59 +0800

    add src

---
+++ src.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhj
+ * Date: 2020/5/25
+ * Time: 17:46
+ */
+
+namespace SCLZZHJ\Schema;
+
+class Schema
+{
+    public function __construct()
+    {
+
+    }
+}
