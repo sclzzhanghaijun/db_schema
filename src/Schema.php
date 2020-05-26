d54b6179a49b24e7657d8d5b032a6425400176f0
xiaohai <sclzzhanghaijun@163.com>
2020-05-26 09:42:11 +0800

    修改返回字段

--- src/Schema.php
+++ src/Schema.php
@@ -9,7 +9,6 @@
 namespace SCLZZHJ\Schema;
 
 
-
 class Schema
 {
     /**
@@ -51,32 +50,38 @@ class Schema
         }
         $html = '<table class="table_schema">';
         foreach ($tables as $key => $rowTable) {
-            if ($key > 1) {
+            if ($key > 0) {
                 $html .= '<tr>';
-                $html .= '<td colspan="4">&nbsp;</td>';
+                $html .= '<td colspan="7">&nbsp;</td>';
                 $html .= '</tr>';
             }
             $html .= '<tr class="table_name" style="font-weight: bold">';
             $html .= '<td>' . $rowTable['TABLE_NAME'] . '</td>';
-            $html .= '<td colspan="3">' . $rowTable['TABLE_COMMENT'] . '</td>';
+            $html .= '<td colspan="6">' . $rowTable['TABLE_COMMENT'] . '</td>';
             $html .= '</tr>';
             $html .= '<tr>';
             $html .= '<td>字段名称</td>';
             $html .= '<td>字段类型</td>';
+            $html .= '<td>默认值</td>';
+            $html .= '<td>是否为空</td>';
+            $html .= '<td>列KEy</td>';
             $html .= '<td>字段描述</td>';
             $html .= '<td>备注</td>';
             $html .= '</tr>';
             $sqlColumn = "SELECT * FROM information_schema.`COLUMNS` WHERE `TABLE_SCHEMA` = '{$this->dbName}' AND `TABLE_NAME` = '{$rowTable['TABLE_NAME']}'"; //构建查询语句
             $queryColumns = $this->sqlExecCallBack->SqlExec($sqlColumn);
             if (!is_array($queryColumns)) {
-
+                throw  new SchemaException("获取数据表信息不是数组");
             }
             foreach ($queryColumns as $rowColumn) {
                 $html .= '<tr>';
                 $html .= '<td>' . $rowColumn['COLUMN_NAME'] . '</td>';
                 $html .= '<td>' . $rowColumn['COLUMN_TYPE'] . '</td>';
+                $html .= '<td>' . $rowColumn['COLUMN_DEFAULT'] . '</td>';
+                $html .= '<td>' . $rowColumn['IS_NULLABLE'] . '</td>';
+                $html .= '<td>' . $rowColumn['COLUMN_KEY'] . '</td>';
                 $html .= '<td>' . $rowColumn['COLUMN_COMMENT'] . '</td>';
-                $html .= '<td>&nbsp;</td>';
+                $html .= '<td>' . $rowColumn['EXTRA'] . '</td>';
                 $html .= '</tr>';
             }
         }
